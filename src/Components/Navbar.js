cdd00ba58f6301e9f4af58ca728608e3fcb2478b
cheedieebeogu <cheedie.ebeogu@gmail.com>
2022-01-09 20:02:52 +0100

    debugging

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ const NavBar = ({ showLogo }) => {
   return (
     <>
       <div className="navlinks-contents">
-        <div className="navlinks-container">
+        <div className="navlinks-container s">
           {showLogo ? (
             <img src={logo} alt="logo" className="full-post-logo" />
           ) : null}
